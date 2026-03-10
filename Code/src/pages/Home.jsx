9beeabfcd1298de27656e814994d625d21c4a209
mthsz-dm <matheusdiasmendespop@gmail.com>
2026-03-10 13:38:18 -0300

    adição de algumas traduções

--- Code/src/pages/Home.jsx
+++ Code/src/pages/Home.jsx
@@ -1,15 +1,16 @@
-import { Link } from 'react-router-dom';
-import '../assets/css/Home.css';
+import { Link } from "react-router-dom";
+import "../assets/css/Home.css";
 
 function Home() {
   return (
     <div className="home">
       <div className="hero-section">
         <h1 className="hero-title">Olá, eu sou [Nome do Autor]</h1>
-<p className="hero-description">
- Este é um portfólio acadêmico desenvolvido para apresentar projetos, habilidades
-  e experiências adquiridas ao longo da formação em Engenharia de Software.
-</p>
+        <p className="hero-description">
+          Este é um portfólio acadêmico desenvolvido para apresentar projetos,
+          habilidades e experiências adquiridas ao longo da formação em
+          Engenharia de Software.
+        </p>
         <Link to="/sobre" className="cta-button">
           Sobre Mim
         </Link>
